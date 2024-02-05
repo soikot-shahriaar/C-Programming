e837bd78beba823d662c69edf7bb471bd46a059f
Shahriar <soikot.shahriaar@gmail.com>
2024-02-06 01:39:01 +0600

    Updated

--- 07 Recursion/pb-06-climbing-stairs-triple.c
+++ 07 Recursion/pb-06-climbing-stairs-triple.c
@@ -4,10 +4,10 @@
 
 int stairWays(int n)
 {
-    if (n == 0)
-        return 1; 
-    else if (n == 1 || n == 2)
-        return n;
+    if (n == 0 || n == 1)
+        return 1;
+    else if (n == 2)
+        return 2;
     else
         return stairWays(n - 1) + stairWays(n - 2) + stairWays(n - 3);
 }
